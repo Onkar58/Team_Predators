28f2b4ad10e1d67757da73230df0eb2ab2c152ad
Onkar Waghmode <onkar58@Onkars-MacBook-Air.local>
2024-09-25 18:25:35 +0530

    fix: removede gallery rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import React from 'react'
-import {
-  Homepage,
-  Contact,
-  Partners,
-  Achievements,
-  Teams,
-  Moments,
-  GalleryPage
-} from './pages'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar/Navbar'
-import Footer from './Components/Footer/Footer'
-import PartnerCrit from './Components/Partners/PartnerCrit'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+import PartnerCrit from "./Components/Partners/PartnerCrit";
+import { Achievements, Contact, Homepage, Partners, Teams } from "./pages";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/achievements' element={<Achievements />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='/partners' element={<Partners />} />
-        <Route path='/teams' element={<Teams/>} />
-        <Route path='/partners/criteria' element={<PartnerCrit/>} />
-        <Route path='/gallery' element={<GalleryPage/>} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/achievements" element={<Achievements />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/partners" element={<Partners />} />
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/partners/criteria" element={<PartnerCrit />} />
       </Routes>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
